5ee2263216b941bd9340698018decdf63f9473c7
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-10-19 21:52:20 +0530

    Adding the most optimal approach

--- 19thOct2023/Backspace_String_Compare.cpp
+++ 19thOct2023/Backspace_String_Compare.cpp
@@ -70,4 +70,52 @@ public:
         }
         return true;
     }
+    //Most optimal approach in terms of the space complexity
+    bool backspaceCompare(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        //Iterating from backwards
+        int i = m-1, j = n-1;
+        int skip_s = 0;
+        int skip_t = 0;
+        
+        while(i >= 0 || j >= 0) {
+            
+            while(i >= 0) {
+                if(s[i] == '#') {
+                    skip_s++;
+                    i--;
+                } else if(skip_s > 0) {
+                    skip_s--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            
+            while(j >= 0) {
+                if(t[j] == '#') {
+                    skip_t++;
+                    j--;
+                } else if(skip_t > 0) {
+                    skip_t--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            
+            char first  = i < 0 ? '$' : s[i];
+            char second = j < 0 ? '$' : t[j];
+            
+            if(first != second)
+                return false;
+            
+            i--;
+            j--;
+        }
+        
+        return true;
+        
+    }
 };
