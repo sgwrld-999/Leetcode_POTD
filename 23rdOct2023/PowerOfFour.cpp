3885735410fdb495baa4a9aab72f153cb0564162
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-10-23 12:29:13 +0530

    Two more ways  to solve the POTD

--- 23rdOct2023/PowerOfFour.cpp
+++ 23rdOct2023/PowerOfFour.cpp
@@ -3,9 +3,29 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
+    bool isPowerOfFourApproach3(int n){
+        //Using the log property
+        if(n == 0) return false;
+        if(n == 1) return true;
+        int x = log(n)/log(4);
+        if(n == pow(4,x)) return true;
+        return false;
+    }
+    bool isPowerOfFourApproach2(int n){
+        if(n == 0){
+            return false;
+        }else if(n == 1){
+            return true;
+        }
+        while(n%4 == 0){
+            n /= 4 ;
+        }
+        if(n == 1) return true;
+        return false ;
+    }
+    bool isPowerOfFourApproach1(int n) {
         //to get x which is n=4^x 
-        int x=log(pow(2,31))/log(4);
+        int x=log(pow(2,31))/log(4); // Maximum of the range of int is 2^31-1 that will be 4^x can hold
         int low = 0 , high = x;
         while(low <= high)
         {
@@ -26,7 +46,7 @@ int main() {
     int n;
     cout << "Enter a number: ";
     cin >> n;
-    if (sol.isPowerOfFour(n)) {
+    if (sol.isPowerOfFourApproach3(n)) {
         cout << n << " is a power of four." << endl;
     } else {
         cout << n << " is not a power of four." << endl;
